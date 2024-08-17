42fc3392314b331f8a3396ddd2347e3aba801214
Craig <craig_jeffcoat@yahoo.com>
2024-08-17 08:39:40 +0100

    Initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>You can reach us at craig@cratergames.co.uk or peter@cratergames.co.uk</p>
+    </div>
+  );
+}
+
+export default Contact;
