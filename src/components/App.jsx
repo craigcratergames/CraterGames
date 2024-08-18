432ce0a16bace59b940fe42f6e957bca1328e15c
Craig <craig_jeffcoat@yahoo.com>
2024-08-18 08:29:10 +0100

    Footer issue resolved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Home";
 import About from './About';
 import Contact from './Contact';
 import NavBar from './NavBar';
 import Footer from './Footer';
 
-
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App d-flex flex-column min-vh-100">
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
